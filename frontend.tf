9d103de9835db4dfb57e124ff706b16b7e4df154
Greg <gregcourt10@gmail.com>
2025-02-15 20:36:56 +0000

    renamed frontend

--- frontend.tf
+++ frontend.tf
@@ -1,5 +1,5 @@
 resource "azurerm_static_web_app" "frontend" {
-  name                = "my-frontend-${var.env}-${var.loc}-01"
+  name                = "webapp-${var.project_name}-${var.env}-${var.loc}-01"
   resource_group_name = azurerm_resource_group.main.name
   location            = "westeurope"
   sku_tier            = "Free"
