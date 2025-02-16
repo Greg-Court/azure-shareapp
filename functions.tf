13f1391f24fe321f350a024554184a6bf90251e7
Greg <gregcourt10@gmail.com>
2025-02-16 09:11:22 +0000

    removed vnet subnet id from functions.tf

--- functions.tf
+++ functions.tf
@@ -70,7 +70,6 @@ resource "azapi_resource" "function_app" {
     }
     properties = {
       serverFarmId = azurerm_service_plan.functions_plan.id
-      virtualNetworkSubnetId = azurerm_subnet.functions.id
 
       functionAppConfig = {
         runtime = {
